c38cc8b44774013c2adda30c93c316a40e85d248
Adam Ross Russell and Marika Allely <student@epicodus.com>
2016-02-16 10:45:01 -0800

    add twig pages

--- app/app.php
+++ app/app.php
@@ -4,9 +4,19 @@
 
     $app = new Silex\Application();
 
-   $app->register(new Silex\Provider\TwigServiceProvider(), array(
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
     	'twig.path' => __DIR__.'/../views'
     ));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('form.html.twig');
+    });
+
+    $app->get('/results', function() use ($app) {
+        $anagram = new Anagram();
+        $result = $anagram->anagramSort($_GET['input'], $_GET['compare_input']);
+        return $app['twig']->render('results.html.twig', array('answer' => $result));
+    });
 
 
     return $app;

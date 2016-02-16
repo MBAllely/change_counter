8e435bd6be6b4d8e476b265305e36ba85badf0c2
Adam Ross Russell and Marika Allely <student@epicodus.com>
2016-02-16 11:36:55 -0800

    add functionality for quarters, nickels, and dimes

--- src/Coins.php
+++ src/Coins.php
@@ -1,7 +1,29 @@
 <?php
     class Coins
     {
-        
+        function coinSort($input)
+        {
+            $quarters = 0;
+            $dimes = 0;
+            $nickels = 0;
+            $pennies = 0;
+
+            while ($input >= 25) {
+                $input -= 25;
+                $quarters += 1;
+            }
+            while ($input >= 10) {
+                $input -= 10;
+                $dimes += 1;
+            }
+            while ($input >= 5) {
+                $input -= 5;
+                $nickels += 1;
+            }
+            return "quarters:" . $quarters . " dimes:" . $dimes . " nickels:" . $nickels;
+
+
+        }
     }
 
  ?>
--- tests/CoinTest.php
+++ tests/CoinTest.php
@@ -15,8 +15,36 @@
             $result = $test_Coin->coinSort($input);
 
             //Assert
-            $this->assertEquals(1, $result);
+            $this->assertEquals("quarters:1 dimes:0 nickels:1", $result);
         }
+
+        function test_coins_dimes()
+        {
+            //Arrange
+            $test_Coin = new Coins;
+            $input = 15;
+
+            //Act
+            $result = $test_Coin->coinSort($input);
+
+            //Assert
+            $this->assertEquals("quarters:0 dimes:1 nickels:1", $result);
+        }
+
+        function test_coins_nickels()
+        {
+            //Arrange
+            $test_Coin = new Coins;
+            $input = 7;
+
+            //Act
+            $result = $test_Coin->coinSort($input);
+
+            //Assert
+            $this->assertEquals("quarters:0 dimes:0 nickels:1", $result);
+        }
+
+
 
     }
 

48b02ba921ef04eb1a42b92deddf3ded7ced4be9
Marika <mballely@gmail.com>
2016-02-17 08:13:20 -0800

    remove debugger and rename project folder

--- app/app.php
+++ app/app.php
@@ -2,12 +2,7 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Coins.php";
 
-    use Symfony\Component\Debug\Debug;
-    Debug::enable();
-
     $app = new Silex\Application();
-
-    $app['debug'] = true;
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     	'twig.path' => __DIR__.'/../views'
